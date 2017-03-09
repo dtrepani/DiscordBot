ca1a09969ef35f1b396ed43ca2e9791f4e8071c9
Fina <finapani@gmail.com>
2017-03-09 16:35:13 -0500

    Added slash and spank commands.

---
+++ src/commands/fun/slash.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Command } = require('discord.js-commando');
+const cleanReply = require('../../modules/clean-reply');
+
+module.exports = class SlashCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'slash',
+			group: 'fun',
+			memberName: 'slash',
+			description: `Take your swords out and slash a user. ⚔️`,
+			examples: ['slash @Kyuu#9384'],
+			guildOnly: true,
+			throttling: {
+				usages: 1,
+				duration: 60
+			},
+			args: [
+				{
+					key: 'member',
+					prompt: 'Slash which user?',
+					type: 'member'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		return cleanReply(
+			msg,
+			`${args.member}, you've been SLASHED by the mighty ${msg.member}!!\nhttp://i.imgur.com/YWHdIKL.gif`
+		);
+	}
+};
---
+++ src/commands/fun/spank.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Command } = require('discord.js-commando');
+const cleanReply = require('../../modules/clean-reply');
+
+module.exports = class SpankCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'spank',
+			group: 'fun',
+			memberName: 'spank',
+			description: `;)`,
+			examples: ['spank @Kyuu#9384'],
+			guildOnly: true,
+			throttling: {
+				usages: 1,
+				duration: 60
+			},
+			args: [
+				{
+					key: 'member',
+					prompt: 'Spank which user?',
+					type: 'member'
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		return cleanReply(
+			msg,
+			`${args.member}, bend over and accept your punishment by ${msg.member}!\nhttp://i.imgur.com/Sqw0WvF.gif`
+		);
+	}
+};
